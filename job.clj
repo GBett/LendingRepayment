a337fb3d2d3a122342e63337ecdc5a9f945275e6
Gilbert Bett <118375989+GBett@users.noreply.github.com>
2023-01-30 12:25:23 +0300

    Create job.clj

---
+++ job.clj
@@ -0,0 +1,72 @@
+(ns clj-jenkins.job
+  (:require [clj-jenkins.build :as build]
+            [clj-jenkins.util :as util]
+            [clj-http.client :as client]
+            [cheshire.core :as json]
+            [clojure.string :as s]))
+
+(declare job-url build-url get-build params->query-string)
+
+(def jenkins-url (util/config-property "JENKINS_URL"))
+(def auth-tokens [(util/config-property "JENKINS_USERNAME") (util/config-property "JENKINS_API_TOKEN")])
+
+(defn get-job [job]
+  (-> job
+    job-url
+    (client/get {:basic-auth auth-tokens})
+    :body (json/parse-string)))
+
+(defn builds [job]
+  ((get-job job) "builds"))
+
+(defn health-report [job]
+  ((first ((get-job job) "healthReport")) "description"))
+
+(defn last-unsuccessful-build [job]
+  (get-build job "lastUnsuccessfulBuild"))
+
+(defn last-build [job]
+  (get-build job "lastBuild"))
+
+(defn last-failed-build [job]
+  (get-build job "lastFailedBuild"))
+
+(defn last-stable-build [job]
+  (get-build job "lastStableBuild"))
+
+(defn last-unstable-build [job]
+  (get-build job "lastUnstableBuild"))
+
+(defn last-successful-build [job]
+  (get-build job "lastSuccessfulBuild"))
+
+(defn upstream-projects [job]
+  ((get-job job) "upstreamProjects"))
+
+(defn downstream-projects [job]
+  ((get-job job) "downstreamProjects"))
+
+(defn trigger-build
+  ([job]
+    (trigger-build job {}))
+  ([job params]
+    (-> (build-url job params)
+      (client/post {:basic-auth auth-tokens
+                    :content-type :json})
+      :body (json/parse-string))))
+
+(defn- get-build [job key]
+  (build/get-build job (((get-job job) key) "number")))
+
+(defn- job-url [job]
+  (str jenkins-url "/job/" job "/api/json"))
+
+(defn build-url [job params]
+  (if (empty? params)
+    (str jenkins-url "/job/" job "/build")
+    (str jenkins-url "/job/" job "/buildWithParameters?" (params->query-string params))))
+
+(defn- params->query-string [params]
+  (s/join "&"
+    (for [[k v] params]
+      (str (name k) "=" (java.net.URLEncoder/encode v)))))
